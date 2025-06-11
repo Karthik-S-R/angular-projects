5dd0d1cd0fe5cb254f3ef088209aabdc8c150a37
Karthik-S-R <karthiksr008@gmail.com>
2025-06-11 21:51:59 +0530

    feat: updated app.routes

--- apps/budget-planner/src/app/app.routes.ts
+++ apps/budget-planner/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'budget-planner',
+    loadChildren: () =>
+      import('./budget-planner/budget-planner.module').then(
+        (m) => m.BudgetPlannerModule
+      ),
+  },
+];
